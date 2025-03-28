4ace4e89be4a5059922d88e1ea3f94ef79a1b14a
PR3C14D0 <aarongarpre@gmail.com>
2025-03-28 03:27:05 +0100

    Solved.

--- frontend/src/pages/blog/[id].tsx
+++ frontend/src/pages/blog/[id].tsx
@@ -92,9 +92,13 @@ const ViewPost: React.FC<{data: {title:string, imgUrl: string, content: string, 
                         h1: ({children}) => <h1 onClick={copyLink} className="pb-8 text-4xl font-fira mt-4 mb-2 flex flex-row items-center group hover:cursor-pointer">
                                 <i className="opacity-0 group-hover:opacity-100 transition fa-solid fa-link text-gray-600 text-xl"></i>
                                 &nbsp;&nbsp;
-                                <div id={(children?.toLowerCase() ?? '').replace(/\s+/g, '-')} className="flex flex-col">
-                                    {children}
-                                </div>
+                                {
+                                    (typeof children === 'string') ? (
+                                        <div id={children.toLowerCase().replace(/\s+/g, '-')} className="flex flex-col">
+                                          {children}
+                                        </div>
+                                      ) : <></>
+                                }
                             </h1>,
                         a: ({children, href}) => <a href={href} className="font-fira text-sky-400 hover:text-sky-600 hover:underline transition">{children}</a>,
                         p: ({children}) => <p className="text-white font-fira text-lg">{children}</p>,
