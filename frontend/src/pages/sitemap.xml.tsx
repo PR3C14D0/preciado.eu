5a3c3dac7fc18b7c2383a74caaa17c46f253be0b
PR3C14D0 <aarongarpre@gmail.com>
2025-05-13 06:11:34 +0200

    Added styles to the sitemap and modified robots.txt

--- frontend/src/pages/sitemap.xml.tsx
+++ frontend/src/pages/sitemap.xml.tsx
@@ -33,6 +33,7 @@ export const getServerSideProps: GetServerSideProps = async ({res}: {res: any}) 
     })
 
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+        <?xml-stylesheet type="text/xsl" href="/xsl/linked.xslt"?>
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
             ${staticPages
                 .map((url) => {
